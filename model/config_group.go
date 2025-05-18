48091ffca39a993ccb493ad731d6d96123e73466
katarinaastevanovic <128474951+katarinaastevanovic@users.noreply.github.com>
2025-05-18 20:34:05 +0200

    Add files via upload

---
+++ model/config_group.go
@@ -0,0 +1,34 @@
+package model
+
+type ConfigGroup struct {
+	Name          string   `json:"name"`
+	Version       int      `json:"version"`
+	Configuration []Config `json:"configuration"`
+}
+
+func NewConfigGroup(name string, version int, configuration []Config) ConfigGroup {
+	return ConfigGroup{
+		Name:          name,
+		Version:       version,
+		Configuration: configuration,
+	}
+}
+
+func NewConfigGroup2(name string, version int) ConfigGroup {
+	return ConfigGroup{
+		Name:    name,
+		Version: version,
+	}
+}
+
+type ConfigGroupRepository interface {
+	Create(configGroup ConfigGroup) error
+	Read(name string, version int) (ConfigGroup, error)
+	Update(configGroup ConfigGroup) error
+	Delete(name string, version int) error
+	GetAll() ([]ConfigGroup, error)
+	Add(ConfigGroup ConfigGroup)
+	Get(name string, version int) (ConfigGroup, error)
+	RemoveConfig(groupName string, groupVersion int, configName string, configVersion int) error
+	AddConfig(groupName string, groupVersion int, config Config) error
+}
